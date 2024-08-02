65257b6040b3ece806ba45a1725bf9600c57ed81
LUIS-HP\soylu <soy.luis63@outloo.com>
2024-08-01 23:56:37 -0600

    cambio de url mongo atlas

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/Pecera', {
+    await mongoose.connect('mongodb+srv://luishernandez21s:33116011Luis@cluster0.mp6adf4.mongodb.net/pecera?', {
      //mongodb://localhost:27017?Pecera
      //'mongodb+srv://luishernandez21s:33116011Luis*@cluster0.0nmr178.mongodb.net/pecera?retryWrites=true&w=majority'
+     //'mongodb+srv://luishernandez21s:33116011Luis@cluster0.mp6adf4.mongodb.net/pecera?'
     });
 
     console.log('Conexión a MongoDB Atlas establecida');
